d935a66fc52f5916feec9e992ab26bf81b2cf77c
박승식 <briankunkel9@gmail.com>
2025-08-27 10:59:51 +0900

    플레이어 이동 UI 연동

--- Assets/Scripts/Player/PlayerMoveController.cs
+++ Assets/Scripts/Player/PlayerMoveController.cs
@@ -1,43 +1,68 @@
 using System;
 using Sirenix.OdinInspector;
 using Unity.Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
-public class PlayerMoveController : MonoBehaviour , InputSystem_Actions.IPlayerActions
+public class PlayerMoveController : MonoBehaviour 
 {
-    [ReadOnly] private Vector2 moveDirection;
+    [SerializeField] private InputActionAsset inputActionAsset;
+    [ReadOnly] private Vector2 moveInputVector;
     [SerializeField] private float moveSpeed = 5f;
-    InputSystem_Actions inputSystemActions;
-    void Start()
+    [SerializeField] private float rotateSpeed = 180f;
+
+    private InputActionMap playerActionMap;
+    private InputAction moveAction;
+    
+    private Camera mainCamera;
+
+    [ReadOnly,ShowInInspector] private Vector3 rightMoveDirection;
+    [ReadOnly,ShowInInspector] private Vector3 forwardMoveDirection;
+    
+    [SerializeField] Animator animator;
+
+    private void Awake()
     {
+        playerActionMap =  inputActionAsset.FindActionMap("Player");
+        
+        moveAction = playerActionMap.FindAction("Move");
+        mainCamera =  Camera.main;
+        
+        rightMoveDirection = mainCamera.transform.right;
+        rightMoveDirection.y = 0;
+        rightMoveDirection = rightMoveDirection.normalized;
+        
+        forwardMoveDirection = mainCamera.transform.forward;
+        forwardMoveDirection.y = 0;
+        forwardMoveDirection = forwardMoveDirection.normalized;
+        
+        
     }
+
 
     private void Update()
     {
-        var moveRight = moveDirection.x * transform.right;
-        var moveForward = moveDirection.y * transform.forward;
-        var moveDelta = (moveRight + moveForward).normalized * moveSpeed * Time.deltaTime;
-        transform.Translate(moveDelta, Space.World); ;
+        moveInputVector = moveAction.ReadValue<Vector2>();
+        
+        var moveRight = moveInputVector.x * rightMoveDirection;
+        var moveForward = moveInputVector.y * forwardMoveDirection;
+        var toward = (moveRight + moveForward).normalized;
+        moveDirection = Vector3.Slerp(moveDirection , toward, Time.deltaTime * rotateSpeed);
+        transform.forward = moveDirection;
+        
+        animator.SetFloat("MoveSpeed", moveInputVector.magnitude);
     }
 
-    private void OnEnable()
-    {
-        inputSystemActions = new InputSystem_Actions();
-        inputSystemActions.Player.SetCallbacks(instance : this);
-        inputSystemActions.Enable();
-    }
+    [ShowInInspector] private Vector3 moveDirection;
     
-    private void OnDisable()
+    private void FixedUpdate()
     {
-        inputSystemActions.Disable();
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveDirection = context.ReadValue<Vector2>();
+        if (moveInputVector != Vector2.zero)
+        {
+            var moveDelta = moveDirection * (moveSpeed * Time.fixedDeltaTime);
+            transform.Translate(moveDelta, Space.World);
+        }
         
     }
 }
